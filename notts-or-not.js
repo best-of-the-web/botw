28a384dda6bb64ef7f7934525d284d85b9856e61
best-of-the-web <gregson33@gmail.com>
2025-12-07 14:48:33 +0000

    1

--- notts-or-not.js
+++ notts-or-not.js
@@ -1,44 +1,15 @@
-let data = [];
-let index = 0;
-let score = 0;
+[
+  { "img": "/pics/notts-or-not/img/notts-or-not-001.jpg", "isNotts": true },
+  { "img": "/pics/notts-or-not/img/notts-or-not-002.jpg", "isNotts": true },
+  { "img": "/pics/notts-or-not/img/notts-or-not-003.jpg", "isNotts": true },
+  { "img": "/pics/notts-or-not/img/notts-or-not-004.jpg", "isNotts": true },
+  { "img": "/pics/notts-or-not/img/notts-or-not-005.jpg", "isNotts": true },
+  { "img": "/pics/notts-or-not/img/notts-or-not-006.jpg", "isNotts": true },
 
-fetch('notts-or-not.json')
-  .then(res => res.json())
-  .then(json => {
-    data = json;
-    loadImage();
-  });
-
-function loadImage() {
-    const item = data[index];
-    document.getElementById('pic').src = item.img;
-    document.getElementById('result').textContent = "";
-    document.getElementById('progress').textContent = `Image ${index + 1} of ${data.length}`;
-}
-
-function guess(choice) {
-    const correct = data[index].isNotts;
-    if (choice === correct) {
-        score++;
-        document.getElementById('result').textContent = "Correct!";
-    } else {
-        document.getElementById('result').textContent = "Wrong!";
-    }
-
-    index++;
-
-    setTimeout(() => {
-        if (index < data.length) {
-            loadImage();
-        } else {
-            endGame();
-        }
-    }, 600);
-}
-
-function endGame() {
-    document.getElementById('game').innerHTML = `
-      <h2>Final Score: ${score} / ${data.length}</h2>
-      <button onclick="location.reload()">Play Again</button>
-    `;
-}
+  { "img": "/pics/notts-or-not/img/notts-or-not-007.jpg", "isNotts": false },
+  { "img": "/pics/notts-or-not/img/notts-or-not-008.jpg", "isNotts": false },
+  { "img": "/pics/notts-or-not/img/notts-or-not-009.jpg", "isNotts": false },
+  { "img": "/pics/notts-or-not/img/notts-or-not-010.jpg", "isNotts": false },
+  { "img": "/pics/notts-or-not/img/notts-or-not-011.jpg", "isNotts": false },
+  { "img": "/pics/notts-or-not/img/notts-or-not-012.jpg", "isNotts": false }
+]
